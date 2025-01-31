fc8d9022f5bdc64ad17fbd33bda086e86410d794
Jaya Krishna Pavan <jkpm4321@gmail.com>
2025-01-31 11:42:06 +0530

    class based and function based components

---
+++ react/day1/app/src/Component/Fbc.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+function Fbc(){
+    return(
+        <h1>This is a function based Component</h1>
+    
+    );
+}
+export default Fbc;
--- react/day1/app/src/main.jsx
+++ react/day1/app/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+// import './index.css'
 import App from './App.jsx'
-
+import Cbc from './Component/Cbc.jsx'
+import Fbc from './Component/Fbc.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Cbc/>
+    <Fbc/>
   </StrictMode>,
 )
