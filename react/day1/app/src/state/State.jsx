a736c327c371ddc892d9771746bcdebd5a5601f4
Jaya Krishna Pavan <jkpm4321@gmail.com>
2025-02-04 11:32:57 +0530

    Updated Day 2 content and modified State.jsx

--- react/day1/app/src/state/State.jsx
+++ react/day1/app/src/state/State.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
 const State = () => {
+    let [name,setName] = useState("Jaya Krishna Pavan")
+    console.log(name);
+    console.log(setName);
+    
+    let demo = ()=>{
+        setName("JK")
+        }
   return (
     <div>
-      State
+        <h1>My name is : {name}</h1>
+        <button onClick={demo}>Click</button>
     </div>
   )
 }
