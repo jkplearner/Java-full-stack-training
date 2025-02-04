9d7aa218e9522c34fb415a96bcce71f2e438b4c6
Jaya Krishna Pavan <jkpm4321@gmail.com>
2025-02-04 11:27:31 +0530

    git commit -m "React: Day 23: Props drilling, default props, states in react"

--- react/day1/app/src/Props/Child.jsx
+++ react/day1/app/src/Props/Child.jsx
@@ -1,25 +1,47 @@
-// import React from 'react';
+// // import React from 'react';
 
-// class Child extends React.Component{
-//     render(){
-//         return(
-//             <>
-//                 <h1>my name is : {this.props.name}</h1>
-//                 <h1>My age is : {this.props.age}</h1>
-//                 <h1>My Education is: {this.props.edu}</h1>
-//                 <h1>My Branch is : {this.props.br}</h1>
-//             </>
-//         )
-//     }
-// }export default Child;
+// // class Child extends React.Component{
+// //     render(){
+// //         return(
+// //             <>
+// //                 <h1>my name is : {this.props.name}</h1>
+// //                 <h1>My age is : {this.props.age}</h1>
+// //                 <h1>My Education is: {this.props.edu}</h1>
+// //                 <h1>My Branch is : {this.props.br}</h1>
+// //             </>
+// //         )
+// //     }
+// // }export default Child;
+// import React from 'react'
+
+// const Child = (props) => {
+//   console.log(props)
+//   let {name, id, designation} = props.obj;
+//   return (
+//     <div>
+//       {/* <h1>my name is : {props.name}</h1> */}
+//       {/* {
+//         props.arr.map((x) => {
+//           return <li>{x}</li>
+//         })
+//       } */}
+//       <h1>{name}<br></br>{id}<br></br>{designation}</h1>
+//     </div>
+//   )
+// }
+
+// export default Child
 import React from 'react'
 
 const Child = (props) => {
   return (
     <div>
-      <h1>my name is : {props.name}</h1>
+      <button onClick={()=>{
+        props.fun()
+      }}>click</button>
     </div>
   )
 }
 
 export default Child
+  
--- react/day1/app/src/Props/Parent.jsx
+++ react/day1/app/src/Props/Parent.jsx
@@ -1,32 +1,54 @@
-// import React from 'react';
-// import Child from './Child.jsx';
-// class Parent extends React.Component{
-//     obj={
-//         username:"M Jaya Krishna Pavan",
-//         Education : "B.Tech",
-//         Branch : "CSE",
-//         Age : 21,
+// // import React from 'react';
+// // import Child from './Child.jsx';
+// // class Parent extends React.Component{
+// //     obj={
+// //         username:"M Jaya Krishna Pavan",
+// //         Education : "B.Tech",
+// //         Branch : "CSE",
+// //         Age : 21,
 
-//     }
-//     render(){
-//         return(
-//             <>
-//                 <Child name ={this.obj.username}
-//                         edu = {this.obj.Education}
-//                         br = {this.obj.Branch}
-//                         age = {this.obj.Age}/>
-//             </>
-//         )
-//     }
-// }export default Parent;
+// //     }
+// //     render(){
+// //         return(
+// //             <>
+// //                 <Child name ={this.obj.username}
+// //                         edu = {this.obj.Education}
+// //                         br = {this.obj.Branch}
+// //                         age = {this.obj.Age}/>
+// //             </>
+// //         )
+// //     }
+// // }export default Parent;
+// import React from 'react'
+// import Child from './Child'
+
+// const Parent = () => {
+//   const obj1 = {
+//     name: "M Jaya Krishna Pavan",
+//     id:1,
+//     designation: "Software Developer",
+//   }
+//   return (
+//     <div>
+//       {/* <Child name="jk"/> */}
+//       {/* <Child id={10}/> */}
+//       {/* <Child arr={[10,20,30,40]} /> */}
+//       <Child obj = {obj1}/>
+//     </div>
+//   )
+// }
+
+// export default Parent
 import React from 'react'
 import Child from './Child'
-
 const Parent = () => {
+  let demo = ()=>{
+    window.alert("I am arrow function")
+  }
   return (
-    <div>
-      <Child name="jk"/>
-    </div>
+    <>
+    <Child fun = {demo}/>
+    </>
   )
 }
 
