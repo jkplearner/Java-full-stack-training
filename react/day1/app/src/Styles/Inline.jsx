cb748ab3a0bf2f69cf4576d92dcbfa9d8d54a47f
Jaya Krishna Pavan <jkpm4321@gmail.com>
2025-02-06 11:40:07 +0530

    clone

---
+++ react/day1/app/src/Styles/Inline.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Inline = () => {
+  return (
+    <>
+        <h1 style={{backgroundColor:"red",
+        textAlign:"center",
+        textDecoration:"underline",
+        textDecorationColor:"yellow",
+        }}>Hello world</h1>
+
+    </>
+  )
+}
+
+export default Inline
--- react/day1/app/src/main.jsx
+++ react/day1/app/src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import "./Styles/globally.css"
 // import './index.css'
 // import App from './App.jsx'
 // import Cbc from './Component/Cbc.jsx'
 // import Fbc from './Component/Fbc.jsx'
 // import Demo from './Component/demo.jsx'
-import State from './state/State'
+import Inline from './Styles/Inline'
+// import State from './state/State'
 // import Parent from './Props/Parent'
+import Comp1 from './Styles/Comp1'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <State/>
+    <Comp1/>
   </StrictMode>,
 )
