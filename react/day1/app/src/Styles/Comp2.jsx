7e8bcc33d0c5d07e6f45ef50480ee28763e68416
Jaya Krishna Pavan <jkpm4321@gmail.com>
2025-02-07 22:31:14 +0530

    clone day 2

--- react/day1/app/src/Styles/Comp2.jsx
+++ react/day1/app/src/Styles/Comp2.jsx
@@ -1,16 +1,42 @@
 import React from 'react'
-import './globally.css'
+
 const Comp2 = () => {
   return (
-    <>
-    <section id='main'>
-      <h1>₹59.00 for 2 months of Premium Student</h1>
-      <h2>Only ₹59.00/month after. Cancel anytime.</h2>
-      <button>Get Premium</button>
-      <p>Offer available only to students at an accredited higher education institution. Offer not available to users who already tried 
-        <br></br>Premium. Spotify Student Discount Offer <a href='#'>Terms and conditions</a> apply.</p>
-    </section>
-    </>
+      <>
+        <section id='main'>
+              <h1>₹59.00 for 2 months of Premium Student</h1>
+              <h2>Only ₹59.00/month after. Cancel anytime.</h2>
+              <button>get premium</button>
+              <p>
+              Offer available only to students at an accredited higher education institution. Offer not available to users who already tried <br /> Premium. Spotify Student Discount Offer <a href="#">Terms and conditions</a> apply.
+              </p>
+        </section>
+        <section id='main1'>
+            <h1>Soundtrack your study</h1>
+            <div id='sub-main'>
+                <div className="demo">
+                  <img src="https://i.scdn.co/image/ab671c3d0000f43005a36f4846dec6dd07cb8f16" alt="" />
+                  <p>Ad-free music listening</p>
+                  <p>Study til sunset. Dance til dawn.</p>
+                </div>
+                <div className="demo">
+                  <img src="https://i.scdn.co/image/ab671c3d0000f430e400ba62f90419741db228a6" alt=""  />
+                  <p>Listen offline</p>
+                  <p>Never worry about data again, wherever you are.</p>
+                </div>
+                <div className="demo">
+                  <img src="https://i.scdn.co/image/ab671c3d0000f4304e19b1ec17b710906741e099" alt="" />
+                  <p>Music for every moment</p>
+                  <p>Millions of songs, amazing playlists.</p>
+                </div>
+                <div className="demo">
+                  <img src="	https://i.scdn.co/image/ab671c3d0000f4302fcaf27c28ba9ca60d638328" alt="" />
+                  <p>Easy on the ears</p>
+                  <p>Premium packs even more sound quality into every song.</p>
+                </div>
+            </div>
+        </section>
+      </>
   )
 }
 
