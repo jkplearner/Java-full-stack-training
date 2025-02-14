d77aef950b62dc964f302eef2d18896a003df9f5
Jaya Krishna Pavan <jkpm4321@gmail.com>
2025-02-14 11:40:21 +0530

    router

--- react/Routing/react-router/src/Component/Dashboard.jsx
+++ react/Routing/react-router/src/Component/Dashboard.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
-
+import { Link,Outlet } from 'react-router-dom'
 const Dashboard = () => {
   return (
     <div>
         <h1>Dashboard Soon....</h1>
+        <Link to='dashboard/mobiles'>Mobiles</Link>
+        <Link to='dashboard/laptops'>laptops</Link>
+        <Outlet></Outlet>
     </div>
   )
 }
--- react/Routing/react-router/src/Component/Master.jsx
+++ react/Routing/react-router/src/Component/Master.jsx
@@ -3,12 +3,17 @@ import { Router,Route,Routes } from 'react-router-dom'
 import Login from './Login'
 import Dashboard from './Dashboard'
 import Error from './Error'
+import Mobiles from './Mobiles'
+import Laptops from './Laptops'
 const Master = () => {
   return (
     <div>
         <Routes>
                 <Route path="/" element={<Login />} />
-                <Route path="/dashboard" element={<Dashboard />} />
+                <Route path="/dashboard" element={<Dashboard />} >
+                  <Route path='dashboard/mobiles' element={<Mobiles/>}></Route>
+                  <Route path='dashboard/laptops' element={<Laptops/>}></Route>
+                </Route>
                 <Route path="/error" element={<Error />} />
         </Routes> 
             
