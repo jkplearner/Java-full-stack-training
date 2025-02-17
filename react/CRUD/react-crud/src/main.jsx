6559b1141bab4e3e3952be0ff56b3c4a888cd329
Jaya Krishna Pavan <jkpm4321@gmail.com>
2025-02-17 11:02:27 +0530

    crud operations

--- react/CRUD/react-crud/src/main.jsx
+++ react/CRUD/react-crud/src/main.jsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-
+// import './index.css'
+// import App from './App.jsx'
+import {BrowserRouter, Routes,Route} from "react-router-dom";
+import Home from './Component/Home';
+import Create from './Component/Create';
+import Edit from './Component/Edit';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/* <App /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/create' element={<Create/>}/>
+        <Route path='/edit/:userid' element={<Edit/>}/>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
